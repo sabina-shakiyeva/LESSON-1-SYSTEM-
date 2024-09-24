9dd88fad5898052fc1db5ae1eb3fe6d43f499a9f
Shaki_bf42 <Shaki_bf42@STHQ0118-16>
2024-09-24 11:47:10 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+namespace lesson1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while(true)
+        {
+            var processes = Process.GetProcesses();
+           
+            for (int i = 1; i < processes.Length; i++)
+            {
+                Console.WriteLine($"{i}){processes[i].ProcessName}");
+            }
+            Console.Write("Choice:");
+            Console.WriteLine("1)New Task");
+            Console.Write("2)End Task");
+            short choice=short.Parse(Console.ReadLine());
+           
+            if(choice == 1)
+            {
+                string processName= Console.ReadLine();
+                var process=Process.Start(processName);
+            }
+            else if (choice == 2)
+            {
+                Console.Write("Enter id for kill process:");
+                int processId=int.Parse(Console.ReadLine());
+                var processs=Process.GetProcessById(processId);
+                processs.Kill();
+               
+                Console.WriteLine("Proses Kill olundu");
+                
+
+            }
+        }
+    }
+}
